02cf40b21bfdbc5de9514f820bedfdea12b4f315
Mike van Genderen <mikevg466@gmail.com>
2019-12-28 22:04:12 -0500

    feat: integrate sorting actions with components
    Add onClick handles with sorting actions to the NameContainers.  Update styling for hovering over clickable names.  Update lifecycle logic to load new selectable names after sorting is complete.

--- src/containers/Root/index.jsx
+++ src/containers/Root/index.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { initializeSortingList, loadNames } from '../../actions/names';
+import {
+  initializeSortingList,
+  loadNames,
+  setSelectorNames,
+} from '../../actions/names';
 import Root from '../../components/Root';
 
 /**
@@ -23,10 +27,19 @@ export class RootContainer extends Component {
    * @param  {Object} prevProps previous component props
    */
   componentDidUpdate(prevProps) {
-    const { initializeSortingList, rawList } = this.props;
-    const { rawList: prevRawList } = prevProps;
+    const {
+      initializeSortingList,
+      rawList,
+      setSelectorNames,
+      sortingList,
+    } = this.props;
+    const { rawList: prevRawList, sortingList: prevSortingList } = prevProps;
     if (rawList !== prevRawList) {
       initializeSortingList();
+    }
+
+    if (sortingList !== prevSortingList) {
+      setSelectorNames();
     }
   }
 
@@ -35,27 +48,32 @@ export class RootContainer extends Component {
    * @return {JSX}
    */
   render() {
-    const { isLoading } = this.props;
-    console.log(isLoading);
-    return <Root isLoading={isLoading} />;
+    const { isLoading, isSortingFinished } = this.props;
+    return <Root isLoading={isLoading} isSortingFinished={isSortingFinished} />;
   }
 }
 
 RootContainer.propTypes = {
   isLoading: PropTypes.bool.isRequired,
+  isSortingFinished: PropTypes.bool.isRequired,
   initializeSortingList: PropTypes.func.isRequired,
   loadNames: PropTypes.func.isRequired,
   rawList: PropTypes.array.isRequired,
+  setSelectorNames: PropTypes.func.isRequired,
+  sortingList: PropTypes.array.isRequired,
 };
 
 export const mapStateToProps = state => ({
   isLoading: !state.names.rawList.length,
+  isSortingFinished: state.names.isSortingFinished,
   rawList: state.names.rawList,
+  sortingList: state.names.sortingList,
 });
 
 export const mapDispatchToProps = {
   initializeSortingList,
   loadNames,
+  setSelectorNames,
 };
 
 export default connect(
